f98d05627146c93a1800c9f4f72ec060aac7ad6e
Andrew May <andrew.may@leadingedje.com>
2019-09-22 10:52:05 -0400

    Configure environment using CDK environment variables

--- aws/cdk/simple-vpc/app.py
+++ aws/cdk/simple-vpc/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 from aws_cdk import core
 
 from simple_vpc.simple_vpc_stack import SimpleVpcStack
 
-dev=core.Environment(account='648758314004', region='us-east-2')
+dev=core.Environment(account=os.environ['CDK_DEFAULT_ACCOUNT'],
+                     region=os.environ['CDK_DEFAULT_REGION'])
 
 app = core.App()
 SimpleVpcStack(app, "simple-vpc", env=dev)
